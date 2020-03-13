f64260ae052c26e8bd1422b135d89c0d29ea2968
Md Milon Ahmed <milon153769@gmail.com>
2020-03-13 09:59:56 +0600

    Register Edited

--- src/com/java/milon/Register_Dao.java
+++ src/com/java/milon/Register_Dao.java
@@ -17,30 +17,41 @@ public class Register_Dao extends HttpServlet {
         String email=request.getParameter("email");
         String gender=request.getParameter("gender");
         String password=request.getParameter("password");
-        int status;
-        try {
-            Connection con= DbConnection.getConnection();
-            PreparedStatement ps=con.prepareStatement("INSERT INTO registration VALUES (?,?,?,?)");
-            ps.setString(1,username);
-            ps.setString(2,email);
-            ps.setString(3,gender);
-            ps.setString(4,password);
-            status=ps.executeUpdate();
+        String country=request.getParameter("country");
+        //int status;
 
-            int code= (int) ((Math.random()*99999)+11111);
-            SendEmail.send(email,"Confirmation code","Welcome to Database Client . Your code here:"+code,SendEmail.TEXT_MAIL);
+        int code= (int) ((Math.random()*99999)+11111);
+        SendEmail.send(email,"Confirmation code","Welcome to Database Client . Your code here:"+code,SendEmail.TEXT_MAIL);
+//        try {
+//            Connection con= DbConnection.getConnection();
+//            PreparedStatement ps=con.prepareStatement("INSERT INTO registration VALUES (?,?,?,?,?)");
+//            ps.setString(1,username);
+//            ps.setString(2,email);
+//            ps.setString(3,gender);
+//            ps.setString(4,password);
+//            ps.setString(5,country);
+//            status=ps.executeUpdate();
+//
+////            int code= (int) ((Math.random()*99999)+11111);
+////            SendEmail.send(email,"Confirmation code","Welcome to Database Client . Your code here:"+code,SendEmail.TEXT_MAIL);
+//
+//            if (status>0){
+                request.getSession().setAttribute("username",username);
+                request.getSession().setAttribute("email",email);
+                request.getSession().setAttribute("gender",gender);
+                request.getSession().setAttribute("password",password);
+                request.getSession().setAttribute("country",country);
 
-            if (status>0){
                 request.getSession().setAttribute("Confirmation_code",code);
                 request.getRequestDispatcher("ConfirmationEmail.jsp").forward(request,response);
-            }else {
-                String msg="Something is wrong please try again";
-                request.setAttribute("msg",msg);
-                request.getRequestDispatcher("Signup.jsp").forward(request,response);
-            }
-        } catch (Exception e) {
-            e.printStackTrace();
-        }
+//            }else {
+//                String msg="Something is wrong please try again";
+//                request.setAttribute("msg",msg);
+//                request.getRequestDispatcher("Signup.jsp").forward(request,response);
+//            }
+//        } catch (Exception e) {
+//            e.printStackTrace();
+//        }
 
     }
 
